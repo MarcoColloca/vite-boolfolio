bc4aef0324aec1e7f0d2de8a9f83e97224b5ff6c
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 15:34:23 +0200

    Add → added AppContact basic structure

---
+++ src/pages/AppContact.vue
@@ -0,0 +1,26 @@
+<script>
+    export default{
+        
+    }
+</script>
+
+
+
+
+
+<template>
+    <h1>
+        Contact
+    </h1>
+    <p>
+        Hello! I am Marco Colloca. 
+    </p>
+    <p>
+        If you want to contact me.. please don't.
+    </p>
+</template>
+
+
+
+<style>
+</style>
