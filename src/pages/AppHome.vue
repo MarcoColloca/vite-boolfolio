6c770d75cd5fa8d425fdb96e76c7bea33244cc04
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 15:34:34 +0200

    Add → added AppHome basic structure

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+        
+    }
+</script>
+
+
+
+
+
+<template>
+    <h1>
+        Home Page
+    </h1>
+    <p>
+        Home.
+    </p>
+</template>
+
+
+
+<style>
+</style>
