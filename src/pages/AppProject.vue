787e7bae968caee2ded9dd2bb0767e2c4a43b18d
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 15:33:05 +0200

    Add → added Project Detail basic page

---
+++ src/pages/AppProject.vue
@@ -0,0 +1,37 @@
+<script>
+    export default{
+
+        mounted(){
+            console.log(this.$router)
+            console.log(this.$route)
+            console.log(this.$route.params.slug);
+        },
+
+        methods:{
+            goBack(){
+                this.$router.back()
+            }
+        }
+    }
+</script>
+
+
+
+
+
+<template>
+    <div class="container">
+        <h1>
+            Project Details
+        </h1>
+    
+        <button class="btn btn-warning" @click="goBack">Indietro</button>
+        
+        <p>{{ $route.params.slug }}</p>
+    </div>
+</template>
+
+
+
+<style>
+</style>
