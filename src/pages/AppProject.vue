37e213c83325a5f28b365cfe530e5842ea4e228e
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 17:44:59 +0200

    Add → axios API call and visualization of single Card

--- src/pages/AppProject.vue
+++ src/pages/AppProject.vue
@@ -1,16 +1,45 @@
 <script>
+    import axios from 'axios';
+    import ProjectCard from '../components/single-components/main/ProjectCard.vue';
+
     export default{
 
+        components:{
+            ProjectCard
+        },
+
+        data(){
+            return{
+                project: {},
+                slug: this.$route.params.slug,
+                description: true
+            }
+        },
+
+
+
         mounted(){
-            console.log(this.$router)
-            console.log(this.$route)
-            console.log(this.$route.params.slug);
+            this.fetchProjects()
+            // console.log(this.$router)
+            // console.log(this.$route)
+            // console.log(this.$route.params.slug);            
         },
 
         methods:{
             goBack(){
                 this.$router.back()
-            }
+            },
+
+
+            fetchProjects(){
+                axios.get(`http://127.0.0.1:8000/api/projects/${this.slug}`)
+                .then(res =>{
+                    // console.log(res.data.results)
+
+                    this.project = res.data.results
+                    
+                })
+            },
         }
     }
 </script>
@@ -20,14 +49,27 @@
 
 
 <template>
-    <div class="container">
+    <div class="container mt-5">
         <h1>
             Project Details
         </h1>
     
-        <button class="btn btn-warning" @click="goBack">Indietro</button>
-        
-        <p>{{ $route.params.slug }}</p>
+        <button class="btn btn-outline-coral" @click="goBack">Back</button>
+                
+    </div>
+
+    <div class="container my-5">
+        <ProjectCard
+         :name="project.name"
+         :description="project.description"
+         :technologies="project.technologies"
+         :link="project.link"
+         :type="project.type?.name"
+         :isPublic="project.is_public"
+         :contributors="project.contributors"
+         :date="project.date_of_creation"
+         :visibleDescription="true"
+        ></ProjectCard>
     </div>
 </template>
 
