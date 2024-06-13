f45487b2fe06aefc562cd3c5c48645a50b120164
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 17:45:14 +0200

    Add → added conditional description to card

--- src/components/single-components/main/ProjectCard.vue
+++ src/components/single-components/main/ProjectCard.vue
@@ -8,6 +8,10 @@
         props:{
             name: String,
             description: String,
+            visibleDescription:{
+                type: Boolean,
+                default: false
+            },
             technologies: Array,
             link: String,
             type: [String, null],
@@ -16,12 +20,18 @@
             date: String,
         },
 
+        
         data(){
             return{
-
+                
             }
         },
+            
 
+
+        mounted(){
+            
+        },
     }
 </script>
 
@@ -33,7 +43,7 @@
         <div class="card-header my-header">
             <h4 class="card-title text-primary">{{ name }}</h4>
         </div>
-        <div class="card-body">
+        <div class="card-body my-body">
             <a :href="link"> Link al Progetto</a>
             <p class="my-3">
                 This Project is <span class="text-coral">{{ isPublic == 0 ? 'Public' : 'Private' }}</span>   
@@ -46,6 +56,9 @@
             </p>
             <p class="my-3">
                 Contributors: {{ contributors }}
+            </p>
+            <p v-show="this.visibleDescription === true">
+                {{ description }}
             </p>
         </div>
         <div class="card-footer my-footer">
@@ -60,10 +73,15 @@
 
 <style lang="scss" scoped>
 
+    .project-card{
+        max-width: 450px;
+    }
+
     .my-header{
         min-height: 100px;
     }
 
+    
 
     .my-footer{
         min-height: 100px;
