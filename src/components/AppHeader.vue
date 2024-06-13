5944e911967b7352984563466d7ad96251738593
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 15:45:16 +0200

    Fix → changed routes fisplayed names

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -18,9 +18,9 @@
     <!-- Page Header -->
     <header>
         <nav class="d-flex gap-5 justify-content-center align-items-center">
-            <RouterLink :to="{name: 'home'}">Go to Home</RouterLink>
-            <RouterLink :to="{name: 'portfolio'}">Go to Blog</RouterLink>
-            <RouterLink to="/contact">Go to About</RouterLink>
+            <RouterLink :to="{name: 'home'}">Home</RouterLink>
+            <RouterLink :to="{name: 'portfolio'}">Portfolio</RouterLink>
+            <RouterLink to="/contact">About Me</RouterLink>
         </nav>
     </header>
 </template>
