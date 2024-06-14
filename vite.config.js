fc1cef5ff962f8b0a3e747a1462a5e47d89aef6b
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-14 09:49:40 +0200

    Fix → changed bootstrap import

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   
-  resolve: {
-    alias: [
-        {
-            // this is required for the SCSS modules
-            find: /^~(.*)$/,
-            replacement: '$1',
-        },
-    ],
-},
+//   resolve: {
+//     alias: [
+//         {
+//             // this is required for the SCSS modules
+//             find: /^~(.*)$/,
+//             replacement: '$1',
+//         },
+//     ],
+// },
 })
