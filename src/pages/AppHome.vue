4955b62da76260e2ed7fe3ef7e2c0d0ce967276f
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-18 13:40:12 +0200

    Add → added Compoent BentoBox with Slot

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,6 +1,10 @@
 <script>
+    import BentoBox from '../components/single-components/general/BentoBox.vue'
+
     export default{
-        
+        components:{
+            BentoBox
+        }
     }
 </script>
 
@@ -17,9 +21,38 @@
             Welcome to my Website. My name is Marco, but my friends call me Mark.  
         </p>
     </div>
+
+    <section class="my-5 hero">
+        <div class="h-100 container">            
+            <div class="hero-container">
+                <h1>
+                    Marco Colloca
+                </h1>
+                <BentoBox class="text-light">
+                    <h2 class="mt-2">
+                        Creative Developer
+                    </h2>
+                </BentoBox>
+            </div>
+        </div>
+    </section>
 </template>
 
 
 
-<style>
+<style lang="scss" scoped>
+    .hero{
+        height: 600px;
+        background-color: antiquewhite;
+        color: black;
+    }
+
+    .hero-container{
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;   
+
+    }
 </style>
